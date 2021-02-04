2d5502308f1e1b34373ab8dbce5fdaf865e35b7a
陆沉 <surgesoft@gmail.com>
2021-02-04 23:58:54 +0800

    chore: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,42 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 
 module.exports = (env, argv) => ({
-    entry: {
-        bundle: './src/index.tsx',
-        ui: './index.ts',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-          }
-        ],
-    },
-    resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
-    plugins: [
-      // Generates an `index.html` file with the <script> injected.
-      new HtmlWebpackPlugin({
-        inject: true,
-        filename: 'ui.html',
-        template: './src/ui.html',
-      }),
-      // Inlines chunks with `runtime` in the name
-      new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/bundle/]),
+  mode: argv.mode === 'production' ? 'production' : 'development',
+
+  // This is necessary because Figma's 'eval' works differently than normal eval
+  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+
+  entry: {
+    ui: './src/index.tsx',
+    code: './index.ts',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
     ],
+  },
+
+  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'global': {} // Fix missing symbol error when running in developer VM
+    }),
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      template: './src/ui.html',
+      filename: 'ui.html',
+      inlineSource: '.(js)$',
+      chunks: ['ui'],
+    }),
+    // Inlines chunks with `runtime` in the name
+    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/bundle/]),
+  ],
 });
