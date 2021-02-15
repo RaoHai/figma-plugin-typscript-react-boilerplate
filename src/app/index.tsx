f3245bce26cfa78e9a951a3041a64bca6c9e73f7
陆沉 <surgesoft@gmail.com>
2021-02-15 12:08:43 +0800

    refactor: add app folder

---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style/index.scss';
+
+export default function App() {
+    return <div className="md:container mx:max-auto"> hello world </div>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/**/*.html',
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
